995d3e76aa2c85a6d9ee23f225a4b9b34c86f907
Aleksandr Efimov <aefimov@elinext.com>
2022-03-25 18:22:54 +0300

    add 2 lecture

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -1,41 +1,39 @@
 /// <reference types="Cypress" />
 
-// describe('Mobile phone replenishment', () => {
+describe('Mobile phone replenishment', () => {
 
-//     context("Replenishment less than the allowed amount", () => {
-//         it("Show error: Minimum amount of the replenishment 1 UAH", () => {});
-//     });
+    context("Replenishment less than the allowed amount", () => {
+        it("Show error: Minimum amount of the replenishment 1 UAH", () => {});
+    });
 
-// });
+});
 
-// it('By ID', () => {
-//     cy.visit("http://www.facebook.com/");
-//     cy.get('email');
-// })
+it('By ID', () => {
+    cy.visit("http://www.facebook.com/");
+    cy.get('email');
+})
 
-// it('By Class', () => {
-//     cy.visit("https://docs.cypress.io/api/commands/get");
-//     cy.get('.DocSearch-Button');
-// })
+it('By Class', () => {
+    cy.visit("https://docs.cypress.io/api/commands/get");
+    cy.get('.DocSearch-Button');
+})
 
-// it('By Tag', () => {
-//     cy.visit("https://docs.cypress.io/api/commands/get");
-//     cy.get('nav');
-// })
+it('By Tag', () => {
+    cy.visit("https://docs.cypress.io/api/commands/get");
+    cy.get('nav');
+})
 
-// it('By Tag Value', () => {
-//     cy.visit("http://www.facebook.com/");
-//     cy.get('[name="pass"]');
-// })
+it('By Tag Value', () => {
+    cy.visit("http://www.facebook.com/");
+    cy.get('[name="pass"]');
+})
 
-// it.only('By Different Tag', () => {
-//     cy.visit("http://www.facebook.com/");
-//     cy.get('[data-testid="open-registration-form-button"][role="button"]');
-//     cy.fi
-// })
+it.only('By Different Tag', () => {
+    cy.visit("http://www.facebook.com/");
+    cy.get('[data-testid="open-registration-form-button"][role="button"]');
+    cy.fi
+})
 
-
-//some comment here
 it("Using Get with find and Eq", () => {
     cy.visit("https://next.privat24.ua/deposit/open")
     cy.get('tbody').find('td').find('div').find('button').eq(0);
