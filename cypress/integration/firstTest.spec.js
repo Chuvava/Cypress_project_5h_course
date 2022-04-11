5edcae8fd5203551aa95f021d5fff888682af1a8
Aleksandr Efimov <aefimov@elinext.com>
2022-04-11 11:58:35 +0300

    add basePage

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -1,14 +1,15 @@
 /// <reference types="Cypress" />
 import { mobileReplenishment } from "../support/pages/mobileReplenishment"
 import { transfers } from "../support/pages/transfers"
+import { basePage } from "../support/pages/basePage"
 
 it.only('Replenishment of Ukraine mobile phone number', () => {
-    cy.visit('https://next.privat24.ua/mobile?lang=en');
+    basePage.open(''https://next.privat24.ua/mobile?lang=en'')
 
     mobileReplenishment.typePhoneNumber('686979712');
-    mobileReplenishment.typeAmount('1');
-    mobileReplenishment.typeDebitCardData('4552331448138217', '0524', '111');
-    mobileReplenishment.submitPayment();
+    basePage.typeAmount('1');
+    basePage.typeDebitCardData('4552331448138217', '0524', '111');
+    basePage.submitPayment();
     cy.wait(3000);
     mobileReplenishment.checkDebitCard('4552 **** **** 8217');
     mobileReplenishment.checkDebitAmountAndCurrency('1', 'UAH');
@@ -17,17 +18,19 @@ it.only('Replenishment of Ukraine mobile phone number', () => {
 });
 
 it('Money transfer between foreign cards', () => {
-    cy.visit('https://next.privat24.ua/money-transfer/card?lang=en')
+    basePage.open('https://next.privat24.ua/money-transfer/card?lang=en');
 
-    transfers.typeDebitCardData('4552331448138217', '0524', '111');
+    basePage.typeDebitCardData('4552331448138217', '0524', '111');
     transfers.typeDebitNameAndSurname('Shayne', 'McConnell');
     transfers.typeRecieverCard('5309233034765085');
-    transfers.typeAmount('300');
+    basePage.typeAmount('300');
     transfers.typeComment('Cypress test');
     cy.wait(2000);
-    transfers.submitPayment();
+    basePage.submitPayment();
     transfers.checkDebitAndRecieverCards('4552 3314 4813 8217', '5309 2330 3476 5085');
     transfers.checkDebitAmountAndTotalAmount('300 UAH');
     transfers.checkDebitComission('89.10 UAH');
     transfers.checkComment('Cypress test');
+    transfers.checkComment('Cypress test');
 });
+
---
+++ cypress/support/pages/basePage.js
@@ -0,0 +1,5 @@
+export class BasePage {
+
+}
+
+export const basePage = new BasePage();
